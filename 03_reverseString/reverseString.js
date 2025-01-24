fa61863db9dc71a215233b75bedcd7c472b63cbe
Ethan7754 <149860534+Ethan7754@users.noreply.github.com>
2025-01-24 16:36:48 -0700

    Complete exercise, pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,12 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let initialArray = string.split("");
+  let reversedArray = initialArray.reverse();
+  let reversedString = reversedArray.join("");
+  return reversedString;
 };
+
+reverseString("hello");
 
 // Do not edit below this line
 module.exports = reverseString;
+
