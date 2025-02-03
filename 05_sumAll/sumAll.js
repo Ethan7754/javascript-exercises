a26fb394d2a80719e983907ed861c39c51fdca5f
Ethan7754 <149860534+Ethan7754@users.noreply.github.com>
2025-02-03 03:15:32 -0700

    Complete exercise five, pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,31 @@
-const sumAll = function() {
+const sumAll = function(numStart, numEnd) {
+  let sum = 0;
+  if (numStart < 0 || numEnd < 0) {
+    return 'ERROR';
+  }
+  
+  if (Number.isInteger(numStart) === false || Number.isInteger(numEnd) === false) {
+    return 'ERROR';
+  }
 
-};
+  if (Number.isNaN(numStart === true) || Number.isNaN(numEnd) === true) {
+    return 'ERROR';
+  }
+
+  if (numStart < numEnd) {
+    for (let i = numStart; i <= numEnd; i++) {
+      sum += i;
+    }
+    return sum;
+  } else if (numStart > numEnd) {
+    for (let i = numEnd; i <= numStart; i++) {
+      sum += i;
+    }
+    return sum;
+  }
+}
+
+
 
 // Do not edit below this line
 module.exports = sumAll;
