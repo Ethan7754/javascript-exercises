366024e830b8741c83d50021ebb6b85885f13c64
Ethan7754 <149860534+Ethan7754@users.noreply.github.com>
2025-02-03 04:07:53 -0700

    Complete exercise six, pass all tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  if (Number.isInteger(year) === true && ((year % 4 === 0) === true) && (((year % 100 === 0) === false) || ((year % 100 === 0) === true && (year % 400 === 0) === true))) {
+    return true;
+  } else {
+    return false;
+  }
 
 };
 
