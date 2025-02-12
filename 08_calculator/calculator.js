29b6def6c28f010a3a2af4e9ca460aa71c335224
Ethan7754 <149860534+Ethan7754@users.noreply.github.com>
2025-02-11 18:53:01 -0700

    Pass all tests, complete exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  const summed = array.reduce((total, currentValue) => total + currentValue, 0);
+  return summed;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  const multiplied = array.reduce((total, currentValue) => total * currentValue);
+  return multiplied;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num > 0 && num !== 1) {
+	  for (let i = num - 1; i > 0; i--) {
+      num *= i; 
+    }
+    return num;
+  } else {
+    return 1;
+  }
 };
 
 // Do not edit below this line
