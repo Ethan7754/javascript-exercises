983978344a31d508805570b6b67dda9edea965e8
Ethan7754 <149860534+Ethan7754@users.noreply.github.com>
2025-02-20 16:21:05 -0700

    Pass all tests, complete exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  const array = [];
+  for (let i = 0; i < 100; i++) {
+    if (array.length === 0 || array.length === 1) {
+      array.push(1);   
+    } else {
+      array.push(array[array.length - 1] + array[array.length - 2]);
+    }
+  } 
+  if (num === 0 || parseInt(num) === 0) {
+    return 0;
+  } else if (num < 0) {
+    return "OOPS";
+  } else if (typeof num === "string") {
+    return array[parseInt(num) - 1];
+  } else {
+    return array[num - 1];
+  }
 };
+  
 
 // Do not edit below this line
 module.exports = fibonacci;
