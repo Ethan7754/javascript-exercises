72efe1d22cb9208b2ca66f3bee96cbf86a622ae2
Ethan7754 <149860534+Ethan7754@users.noreply.github.com>
2025-02-20 18:21:25 -0700

    Pass all tests, complete exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
 
+const palindromes = function (str) {
+  let replacedString = str.replaceAll(/\p{P}/ug, "");
+  let replacedString2 = replacedString.replaceAll(" ", "");
+  let initialArray = replacedString2.split("");
+  let reversedArray = initialArray.reverse();
+  let reversedString = reversedArray.join("");
+  if (replacedString2.toLowerCase() === reversedString.toLowerCase()) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
