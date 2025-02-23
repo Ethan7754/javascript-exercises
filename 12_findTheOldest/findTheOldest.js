c77e8272d94191a265f00a4be73d784b70ce93bd
Ethan7754 <149860534+Ethan7754@users.noreply.github.com>
2025-02-22 23:18:05 -0700

    COmplete exercise 12, complere all object exercises :)

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+  const updated = array.map(person => {
+    if (person.yearOfDeath === undefined) {
+      person = {...person, yearOfDeath: new Date().getFullYear()};
+      return {...person, age: person.yearOfDeath - person.yearOfBirth};
+    } else {
+      return {...person, age: person.yearOfDeath - person.yearOfBirth};
+    }
+   }
+  );
+  const oldestToYoungest = updated.sort(({age: a}, {age: b}) => b-a);
+  return oldestToYoungest[0];
 };
 
 // Do not edit below this line
